7eeca8f1f20acc5f75aaf85a8600d66d0aad6ceb
enrico baiano <s309484@studenti.polito.it>
2025-03-10 16:39:42 +0100

    add provazero

---
+++ provazero.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello, world!" << std::endl;
+    int x = 2;
+    double y = 0.1234;
+    std::cout << "x = " << x << ", y = " << y << std::endl;
+    return 0;
+}
