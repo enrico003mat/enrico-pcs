d814b2b5ec19708dfebf7608e58622062e4ed4de
enrico baiano <s309484@studenti.polito.it>
2025-03-10 16:48:51 +0100

    ho modificato file provazero

--- provazero.cpp
+++ provazero.cpp
@@ -4,6 +4,7 @@ int main(void)
 {
     std::cout << "Hello, world!" << std::endl;
     int x = 2;
+	int a=10;
     double y = 0.1234;
     std::cout << "x = " << x << ", y = " << y << std::endl;
     return 0;
